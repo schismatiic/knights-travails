0740d67e8f056c0bbc3a72e8c0619603354b16bd
schismatiic <leonardojofre2003@gmail.com>
2026-05-30 00:39:55 -0400

    Project finished

--- src/knightMoves.js
+++ src/knightMoves.js
@@ -1,46 +1,47 @@
 import { GraphNode } from "./GraphNode.js";
 import { LinkedList } from "./linked-list/LinkedList.js";
-const list = new LinkedList();
-const calcAdjacent = (start) => {
-  const num1 = start[0];
-  const num2 = start[1];
-  const adjacentList = [];
-  let count = 1;
-  while (count !== 9) {
-    let a;
-    let b;
-    if (count < 3) {
-      a = num1 - 2;
-      b = count % 2 === 0 ? num2 - 1 : num2 + 1;
-    } else if (count < 5) {
-      a = count % 2 === 0 ? num1 + 1 : num1 - 1;
-      b = num2 - 2;
-    } else if (count < 7) {
-      a = num1 + 2;
-      b = count % 2 === 0 ? num2 + 1 : num2 - 1;
-    } else {
-      a = count % 2 === 0 ? num1 - 1 : num1 + 1;
-      b = num2 + 2;
-    }
-    if (a >= 0 && b >= 0 && a < 8 && b < 8) {
-      const adjacentNode = new GraphNode([a, b]);
-      if (
-        list.containsToString([a, b]) !== false &&
-        list.containsToString([a, b]) !== undefined
-      ) {
-        adjacentList.push(list.containsToString([a, b]));
-      } else {
-        adjacentList.push(adjacentNode);
-        list.append(adjacentNode);
-      }
-    }
-    count++;
-  }
-  const node = new GraphNode([0, 0], adjacentList);
-  list.append(node);
-  return adjacentList;
-};
+
 const knightMoves = (start, destination) => {
+  const list = new LinkedList();
+  const calcAdjacent = (start) => {
+    const num1 = start[0];
+    const num2 = start[1];
+    const adjacentList = [];
+    let count = 1;
+    while (count !== 9) {
+      let a;
+      let b;
+      if (count < 3) {
+        a = num1 - 2;
+        b = count % 2 === 0 ? num2 - 1 : num2 + 1;
+      } else if (count < 5) {
+        a = count % 2 === 0 ? num1 + 1 : num1 - 1;
+        b = num2 - 2;
+      } else if (count < 7) {
+        a = num1 + 2;
+        b = count % 2 === 0 ? num2 + 1 : num2 - 1;
+      } else {
+        a = count % 2 === 0 ? num1 - 1 : num1 + 1;
+        b = num2 + 2;
+      }
+      if (a >= 0 && b >= 0 && a < 8 && b < 8) {
+        const adjacentNode = new GraphNode([a, b]);
+        if (
+          list.containsToString([a, b]) !== false &&
+          list.containsToString([a, b]) !== undefined
+        ) {
+          adjacentList.push(list.containsToString([a, b]));
+        } else {
+          adjacentList.push(adjacentNode);
+          list.append(adjacentNode);
+        }
+      }
+      count++;
+    }
+    const node = new GraphNode([0, 0], adjacentList);
+    list.append(node);
+    return adjacentList;
+  };
   const startNode = new GraphNode(start, calcAdjacent(start));
   let final;
   let moves = [];
@@ -77,4 +78,4 @@ const knightMoves = (start, destination) => {
   }
 };
 
-knightMoves([0, 7], [7, 0]);
+knightMoves([3, 3], [4, 3]);
